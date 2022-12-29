beb305644b76067a6b4bb5ba04e97b5cefa0e127
Anveshh99 <anny.m1424@gmail.com>
2022-12-29 17:37:25 +0530

    my commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+
+app.get('/',(req,resp)=>{
+    resp.send(<h1>Hello!!!</h1>)
+});
+
+app.get('/home',(req,resp)=>{
+    resp.send(<h1>Welcome to home</h1>)
+});
+
+app.listen(3000,function() {
+    console.log("Running on 3000 port")
+})
