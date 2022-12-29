fe3c2047df4c22c5499fec81448d03766b06bb22
Anveshh99 <anny.m1424@gmail.com>
2022-12-29 18:51:33 +0530

    done

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express=require('express');
 const app=express();
 
 app.get('/',(req,resp)=>{
-    resp.send(<h1>Hello!!!</h1>)
+    resp.send("<h1>Hello!!!</h1>")
 });
 
 app.get('/home',(req,resp)=>{
-    resp.send(<h1>Welcome to home</h1>)
+    resp.send("<h1>Welcome to home</h1>")
 });
 
 app.listen(3000,function() {
     console.log("Running on 3000 port")
-})
+});
